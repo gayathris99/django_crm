bf9cdfcb14ea4517a97ca1664562eb3b66f18dcf
gayathris99 <gayathri11499@gmail.com>
2021-04-28 19:50:35 +0530

    agent CRUD views completed

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save , pre_save
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
@@ -6,13 +7,15 @@ class User(AbstractUser):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.user.username
- 
+
+
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    organisation = models.ForeignKey(UserProfile, on_delete=models.CASCADE )
     def __str__(self):
         return self.user.email
     
@@ -28,4 +31,11 @@ class Lead(models.Model):
         return self.first_name
 
 
+def post_user_created_signal(sender , instance, created, **kwargs):
+    print(instance,created)
+    if created:
+        UserProfile.objects.create(
+            user = instance
+        )
 
+post_save.connect(post_user_created_signal, sender = User)
